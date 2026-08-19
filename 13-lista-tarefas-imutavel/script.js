c876af4439434deb8995d33ef3e3d890c700f356
FRANCISCO IRANILDO DA SILVA BEZERRA <francisco.isbezerra@senacsp.edu.br>
2026-08-18 22:30:59 -0300

    finalizando aula 13

--- 13-lista-tarefas-imutavel/script.js
+++ 13-lista-tarefas-imutavel/script.js
@@ -7,14 +7,16 @@ const tarefas = [
   { id: 2, titulo: "Praticar arrays", concluida: false },
   { id: 3, titulo: "Revisar funções", concluida: true },
 ];
+console.log(JSON.stringify(tarefas, null, 2))
+
 
 function adicionarTarefa(lista, novaTarefa) {
   // TODO:
   // Retorne um NOVO array incluindo novaTarefa.
-  return{
+  return[
     ...tarefas,
     novaTarefa
-  }
+  ]
   
 }
 
@@ -32,7 +34,18 @@ function concluirTarefa(lista, id) {
 function removerTarefa(lista, id) {
   // TODO:
   // Retorne um NOVO array sem a tarefa informada.
+  return lista.filter((tarefa) => tarefa.id !== id);
 }
+//const ListaSemTarefa = removerTarefa(tarefas,2)
+//console.log(JSON.stringify(ListaSemTarefa, null, 2))
+const comNovaTarefa = adicionarTarefa(tarefas, {
+  id:4,
+  titulo: "estudar imutabilidae",
+  Concluida: false,
+});
+
+const comTarefaConcluida = concluirTarefa(comNovaTarefa, 2);
+const semPrimeiraTarefa = removerTarefa(comTarefaConcluida, 1);
 
 // TODO:
 // Teste as três funções sem alterar diretamente tarefas.
